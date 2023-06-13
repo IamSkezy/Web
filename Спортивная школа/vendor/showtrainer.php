46e7e2184652579c08090d902632438f9ebb966e
IamSkezy <86077283+IamSkezy@users.noreply.github.com>
2023-06-13 12:13:47 +0300

    Update showtrainer.php

--- Спортивная школа/vendor/showtrainer.php
+++ Спортивная школа/vendor/showtrainer.php
@@ -3,16 +3,17 @@
 
     session_start();
     require_once 'connect.php';
-
+    // Получение данных
     $name = trim($_POST['trainer-name']);
-
+    // Запрос к БД
     $check_trainer_info = mysqli_query($link, "SELECT * FROM `trainers` WHERE `trainer_name` = '$name'");
-
+    // Валидация
     if(!empty($name)){
+        // Тренер найден
         if(mysqli_num_rows($check_trainer_info) > 0) {
 
             $trainer_info = mysqli_fetch_assoc($check_trainer_info);
-
+            // Создание сессии
             $_SESSION['trainer_info'] = [
                 "trainer_id" => $trainer_info['trainer_id'],
                 "trainer_name" => $trainer_info['trainer_name'],
