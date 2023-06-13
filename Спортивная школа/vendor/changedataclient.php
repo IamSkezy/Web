a9c2636b0a4bd60e7465e2bf46bbaed40af5b520
IamSkezy <86077283+IamSkezy@users.noreply.github.com>
2023-06-13 12:12:35 +0300

    Update changedataclient.php

--- Спортивная школа/vendor/changedataclient.php
+++ Спортивная школа/vendor/changedataclient.php
@@ -4,11 +4,11 @@
 
     session_start();
     require_once 'connect.php';
-
+    // Получение данных
     $client_id = trim($_POST['id']);
     $name = $_POST['data-change'];
     $data = trim($_POST['change-info']);
-
+    // Условия для отправки корректного запроса к БД
     if($name == 'ФИО') {
         $name = 'client_name';
     }
@@ -27,8 +27,9 @@
     elseif ($name == 'Пароль'){
         $name = 'client_password';
     }
-
+    // Валидация
     if(!empty($name) && !empty($client_id) && !empty($data)){
+        // Изменение данных
         mysqli_query($link, "UPDATE `clients` SET $name = '$data' WHERE `client_id` = '$client_id';");
         $_SESSION['request-status-change-client'] = 'Успешно!'; 
         header('Location: ../profile-admin.php');
