8fb1e2e37f35398b542c9e10040b50f713a336fd
IamSkezy <86077283+IamSkezy@users.noreply.github.com>
2023-06-13 12:16:38 +0300

    Update deletetrainer.php

--- Спортивная школа/vendor/deletetrainer.php
+++ Спортивная школа/vendor/deletetrainer.php
@@ -3,10 +3,11 @@
 
     session_start();
     require_once 'connect.php';
-
+    // Получение данных
     $id = trim($_POST['id']);
-
+    // Валидация
     if(!empty($id)){
+        // Удаление тренера
         mysqli_query($link, "DELETE FROM `trainers` WHERE trainers.trainer_id = '$id';");
         $_SESSION['request-status-delete-trainer'] = 'Успешно!'; 
         header('Location: ../profile-admin.php');
