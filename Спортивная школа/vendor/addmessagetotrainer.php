df774f24a379b45f0a6a1a0eedd679b379b5d2e9
IamSkezy <86077283+IamSkezy@users.noreply.github.com>
2023-06-13 12:03:20 +0300

    Update addmessagetotrainer.php

--- Спортивная школа/vendor/addmessagetotrainer.php
+++ Спортивная школа/vendor/addmessagetotrainer.php
@@ -2,21 +2,22 @@
 <?php
     session_start();
     require_once 'connect.php';
-
+    // Получение данных
     $client_id = $_SESSION['user']['client_id'];
 
     $trainer = $_POST['trainer-name'];
     $date = $_POST['date'];
     $mes = $_POST['message'];
-
+    // Запрос к БД
     $check_trainer = mysqli_query($link, "SELECT * FROM `trainers` WHERE `trainer_name` = '$trainer'");
 
     foreach($check_trainer as $trainers_id){
 		
 		$trainer_id = $trainers_id['trainer_id'];
 	}
-
+    // Валидация на пустые значения
     if(!empty($trainer) and !empty($date) and !empty($mes)) {
+	// Отправка сообщения
         mysqli_query($link, "INSERT INTO `messages` (`message_id`, `trainer_id`, `client_id`, `message_date`, `message_message`, `message_from`) VALUES (NULL, '$trainer_id', '$client_id', '$date', '$mes', 'клиент')");
             $_SESSION['request-status-mes-add-client'] = 'Успешно!'; 
             header('Location: ../profile-client.php');
