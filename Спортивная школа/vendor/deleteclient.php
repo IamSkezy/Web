fe1620a2b5a172b1f55d992e149f48230ff09cdd
IamSkezy <86077283+IamSkezy@users.noreply.github.com>
2023-06-13 12:17:12 +0300

    Update deleteclient.php

--- Спортивная школа/vendor/deleteclient.php
+++ Спортивная школа/vendor/deleteclient.php
@@ -3,10 +3,11 @@
 
     session_start();
     require_once 'connect.php';
-
+    // Получение данных
     $id = trim($_POST['id']);
-
+    // Валидация
     if(!empty($id)){
+        // Удаление клиента
         mysqli_query($link, "DELETE FROM `clients` WHERE clients.client_id = '$id';");
         $_SESSION['request-status-delete-client'] = 'Успешно!'; 
         header('Location: ../profile-admin.php');
