7834a2af3efb9ae0eff655fd29418580bd53f93e
IamSkezy <86077283+IamSkezy@users.noreply.github.com>
2023-06-14 15:14:49 +0300

    Update deleteadmin.php

--- Спортивная школа/vendor/deleteadmin.php
+++ Спортивная школа/vendor/deleteadmin.php
@@ -3,13 +3,15 @@
 
     session_start();
     require_once 'connect.php';
-
+    // Получение данных
     $id = trim($_POST['id']);
 
     $admin_id = $_SESSION['user']['admin_id'];
-
+    // Валидация, чтобы админ не смог удалить сам себя
     if($admin_id != $id) {
+        // Валидация данных
         if(!empty($id)){
+            // Удаление админа
             mysqli_query($link, "DELETE FROM `admins` WHERE admins.admin_id = '$id';");
             $_SESSION['request-status-delete-admin'] = 'Успешно!'; 
             header('Location: ../profile-admin.php');
