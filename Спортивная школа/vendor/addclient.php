df5073d8f27c87f89961c15438b3553fc80f07a0
IamSkezy <86077283+IamSkezy@users.noreply.github.com>
2023-06-14 15:05:37 +0300

    Update addclient.php

--- Спортивная школа/vendor/addclient.php
+++ Спортивная школа/vendor/addclient.php
@@ -3,15 +3,16 @@
 
     session_start();
     require_once 'connect.php';
-
+    // Получение данных
     $name = trim($_POST['name']);
     $email = trim($_POST['email']);
     $address = trim($_POST['address']);
     $phone = trim($_POST['phone']);
     $login = trim($_POST['login']);
     $password = trim($_POST['password']);
-
+    // Валидация данных
     if(!empty($login) && !empty($password) && !empty($email) && !empty($address) && !empty($phone) && !empty($name)){
+        // Добавление клиента
         mysqli_query($link, "INSERT INTO `clients` (`client_id`, `client_name`, `client_email`, `client_address`, `client_phone`, `client_login`, `client_password`) VALUES (NULL, '$name', '$email', '$address', '$phone', '$login', '$password')");
         $_SESSION['request-status-add-client'] = 'Успешно!'; 
         header('Location: ../profile-admin.php');
