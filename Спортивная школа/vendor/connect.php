c53845dcf4be25ae637e2953087c7e9559ff39a5
IamSkezy <86077283+IamSkezy@users.noreply.github.com>
2023-06-14 15:11:21 +0300

    Update connect.php

--- Спортивная школа/vendor/connect.php
+++ Спортивная школа/vendor/connect.php
@@ -1,16 +1,16 @@
 <!-- Файл для подключения к БД -->
 <?php
-
+// Подготовка данных
 $host = 'localhost';
 $user = 'root';
 $password = '';
 $db_name = 'sportfitness';
-
+// Подключение
 $link = mysqli_connect($host, $user, $password, $db_name);
 if($link->connect_errno){
     exit('Error with data base!');
 }
-
+// Кодировка
 $link->set_charset('utf8');
 
 ?>
