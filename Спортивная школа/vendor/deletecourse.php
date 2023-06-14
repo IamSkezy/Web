0dfee387fb727ea89a1087cbaf1c99b1cb097aa6
IamSkezy <86077283+IamSkezy@users.noreply.github.com>
2023-06-14 15:15:58 +0300

    Update deletecourse.php

--- Спортивная школа/vendor/deletecourse.php
+++ Спортивная школа/vendor/deletecourse.php
@@ -3,10 +3,11 @@
 
     session_start();
     require_once 'connect.php';
-
+    // Получение данных
     $id = trim($_POST['id']);
-
+    // Валидация данных
     if(!empty($id)){
+        // Удаление курса
         mysqli_query($link, "DELETE FROM `courses` WHERE courses.course_id = '$id';");
         $_SESSION['request-status-delete-course'] = 'Успешно!'; 
         header('Location: ../profile-admin.php');
