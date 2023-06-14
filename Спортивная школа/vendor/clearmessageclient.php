9d43b28c14dbfa19668fd647800c01d8871404bf
IamSkezy <86077283+IamSkezy@users.noreply.github.com>
2023-06-14 15:10:27 +0300

    Update clearmessageclient.php

--- Спортивная школа/vendor/clearmessageclient.php
+++ Спортивная школа/vendor/clearmessageclient.php
@@ -3,9 +3,9 @@
 
     session_start();
     require_once 'connect.php';
-
+    // Получение данных
     $client_id = $_SESSION['user']['client_id'];
-
+    // Очистка сообщений
     mysqli_query($link,"DELETE FROM `messages` WHERE `client_id` = '$client_id' AND `message_from` = 'тренер';");
     $_SESSION['request-status-delete-mes'] = 'Успешно!'; 
     header('Location: ../profile-client.php');
