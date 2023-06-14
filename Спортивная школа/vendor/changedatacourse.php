4008383f4640888c3b4eed2c0680df9634c9a6b7
IamSkezy <86077283+IamSkezy@users.noreply.github.com>
2023-06-14 15:07:29 +0300

    Update changedatacourse.php

--- Спортивная школа/vendor/changedatacourse.php
+++ Спортивная школа/vendor/changedatacourse.php
@@ -4,11 +4,11 @@
 
     session_start();
     require_once 'connect.php';
-
+    // Получение данных
     $course_id = trim($_POST['id']);
     $name = $_POST['data-change'];
     $data = trim($_POST['change-info']);
-
+    // Условия для отправки коррректного запроса к БД
     if($name == 'Название') {
         $name = 'course_name';
     }
@@ -24,8 +24,9 @@
     elseif ($name == 'Время проведения'){
         $name = 'course_time';
     }
-
+    // Валидация данных
     if(!empty($name) && !empty($course_id) && !empty($data)){
+        // Изменение информации
         mysqli_query($link, "UPDATE `courses` SET $name = '$data' WHERE `course_id` = '$course_id';");
         $_SESSION['request-status-change-course'] = 'Успешно!'; 
         header('Location: ../profile-admin.php');
